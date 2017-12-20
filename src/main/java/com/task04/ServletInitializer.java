a23798916e909a61f6b8f23f70a123c4613278c3
beak <tadanmf@gmail.com>
2017-12-20 18:38:39 +0900

    upload task04

---
+++ src/main/java/com/task04/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.task04;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Task04Application.class);
+	}
+
+}
